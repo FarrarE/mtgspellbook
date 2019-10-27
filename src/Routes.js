d28d94a69648d4250c50cd26ce2d076c27228b51
urza <efarrar@pdx.edu>
2019-10-26 20:28:53 -0700

    Added Login component and route.

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Containers/Home.js";
 import NotFound from "./Containers/NotFound.js";
+import Login from "./Components/Login";
 
 function Routes() {
   return (
     <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
         <Route component={NotFound} />
     </Switch>
   );

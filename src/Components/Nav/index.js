524136694f7936b6bd7bec57a4fac648582cd052
urza <efarrar@pdx.edu>
2019-11-07 13:09:29 -0800

    Fixed narvbar collapse and switched to react-bootstrap

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -6,41 +6,39 @@ import {
     Navbar,
     NavbarBrand,
     Nav,
-    NavItem } from 'reactstrap';
+    NavItem } from 'react-bootstrap';
 
 
 const NavigationMenu = (props) => {
 
   return (
-    <Navbar className="navbar" color="dark" light expand="md">
-        <NavbarBrand>
+    <Navbar className="nav" bg="dark" expand="lg">
+        <Navbar.Brand href="#home">
             <LinkContainer to="/"><h1>MTGSpellBook</h1></LinkContainer>
-        </NavbarBrand>
-
-        <Collapse navbar>
-            <Nav className="ml-auto" navbar>
-                {props.isAuthenticated
-                    ? <>
-                        <LinkContainer to="/collection">
-                            <NavItem><h3>My Collection</h3></NavItem>
-                        </LinkContainer>
-                        <LinkContainer to="/my-deck">
-                            <NavItem><h3>My Decks</h3></NavItem>
-                        </LinkContainer>
-                        <NavItem onClick={props.handleLogout}><h3>Logout</h3></NavItem>
-                    </>
-                    : <>
-                        <LinkContainer to="/signup">
-                        <NavItem><h3>Signup</h3></NavItem>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                        <NavItem><h3>Login</h3></NavItem>
-                        </LinkContainer>
-                    </>
-                }
-            </Nav>
-        </Collapse>
-    </Navbar>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            {props.isAuthenticated
+                ? <>
+                    <LinkContainer to="/collection">
+                        <NavItem><h3>My Collection</h3></NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/my-deck">
+                        <NavItem><h3>My Decks</h3></NavItem>
+                    </LinkContainer>
+                    <NavItem onClick={props.handleLogout}><h3>Logout</h3></NavItem>
+                </>
+                : <>
+                    <LinkContainer to="/signup">
+                    <NavItem><h3>Signup</h3></NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                    <NavItem><h3>Login</h3></NavItem>
+                    </LinkContainer>
+                </>
+            }
+        </Navbar.Collapse>
+        </Navbar>
   )
 }
 

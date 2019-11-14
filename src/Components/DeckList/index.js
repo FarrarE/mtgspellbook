aeac5db908288cb145dfd59e3f12f538f27e34ff
urza <efarrar@pdx.edu>
2019-11-14 01:10:39 -0800

    Removed render options and simplified component

--- src/Components/DeckList/index.js
+++ src/Components/DeckList/index.js
@@ -1,69 +1,19 @@
 import React, { useEffect, useState } from "react";
-import {Col, Row, Button, Form } from "react-bootstrap";
-import { API } from "aws-amplify";
 import Card from '../Card';
 import './styles.css';
 
 export default function DeckList(props) {
-  const [deck, setDeck] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    async function onLoad() {
-      if (!props.isAuthenticated) {
-        return;
-      }
-  
-      try {
-        const deck = await loadDecks();
-        setDeck(deck);
-      } catch (e) {
-        alert(e);
-      }
-  
-      setIsLoading(false);
-    }
-  
-    onLoad();
-  }, [props.isAuthenticated]);
-
-  function loadDecks() {
-    return API.get("notes", "/notes");
-  }
-
-  function renderDeckList(deck) {
-    return (
-      <div className="list">
-        { 
-          props.deckList ? props.deckList.map(card => 
-            <Card card={card} />
-          ) : renderLander()
-        }
-      </div>
-    );
-  }
-
-  function renderLander() {
-    return (
-      <div className="lander">
-        <p>Your spellbook is empty, add a card!</p>
-      </div>
-    );
-  }
-
-  function renderDecks() {
-    return (
-        <div>
-          {!isLoading && renderDeckList(deck)}
-        </div>
-
-    );
-  }
+      const temp = props.list;
+      setList(temp);
+  }, [props.list]);
 
   return (
     <div className="deck-list">
         {props.header === "Main Board" ? <h3>{props.header}</h3> : <h4>{props.header}</h4>}
-        {props.isAuthenticated ? renderDecks() : renderLander()}
+        {list.map(card => <Card card={card} />)}
     </div>
   );
 }
